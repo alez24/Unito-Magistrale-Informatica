4297eac07b3b8eff833ebb01ae4b92d2f552b675
alez24 <alessandrolivero6@gmail.com>
2026-03-06 14:26:43 +0100

    aggiornamento intelligenza

--- intelligenza_artificiale/prolog/listeconcut.pl
+++ intelligenza_artificiale/prolog/listeconcut.pl
@@ -1,15 +1,33 @@
-intersez([],_,[]).
-intersez(_,[],[]).
-intersez([Head|Tail],L2,Head[ITemp]):-
-    member(Head,L2),
-    intersez(Tail,L2,ITemp).
-intersez([_!1|Tail],L2,ITemp):-
-    intersez(Tail,L2,ITemp).
+/* tuttiPositivi(Lista) 
+   true se e solo se tutti gli elementi di Lista sono > 0
+*/
+tuttiPositivi([]).
+tuttiPositivi([Head|Tail]):-
+    atomic(Head),
+    Head > 0,
+    tuttiPositivi(Tail).
+tuttiPositivi([Head|Tail]):-
+    is_list(Head),
+    tuttiPositivi(Head),
+    tuttiPositivi(Tail).
 
-% unione : unione(+L1,+L2,-LU)
-unione([],L2,L2).
-unione(L1,[],L1).
+/* contaPositivi(+Lista,-Quanti) */
+contaPositivi([],0).
+contaPositivi([Head|Tail],Risultato):-
+    Head > 0,!,
+    contaPositivi(Tail,Tot),
+    Risultato is Tot+1.
+contaPositivi([_|Tail],Tot):-
+    contaPositivi(Tail,Tot).
 
-unione([Head|Tail],L2,Head[UTemp]):-
-    member(Head,L2),
-    unione(Tail,L2,UTemp).
+/* sottolista(+L,-S).
+   Esempio: [[1,2,3],1,[2,3],ciccio,[fa,do],-67]
+   Successo con [fa,do]
+   Fallimento con -67
+   Fallimento con [1,[2,3]]
+*/
+sottolista([Head|_],Head):-is_list(Head),!.
+sottolista([_|Tail],R):-sottolista(Tail,R).
+
+prova(X):-member(X,[1,2,3,4,5]),!,member(X,[4,5,10,50]).
+prova(152).
